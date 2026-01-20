c5d1043248a41afaa63ec7d974bb9ef570827a32
devteachercelia <theteachercelia@gmail.com>
2026-01-20 21:30:18 +0100

    Logic of orientation and movement functions

--- src/main/kotlin/com/teachercelia/robocleaner/domain/MoTheRobot.kt
+++ src/main/kotlin/com/teachercelia/robocleaner/domain/MoTheRobot.kt
@@ -5,14 +5,37 @@ class MoTheRobot(
     val orientation: Orientation
 )
 {
-    // TODO: logic of turnRight
 
-    // TODO: logic of turnLeft
+    fun turnRight(robotActualOrientation: Orientation): Orientation{
+        return when(robotActualOrientation){
+            Orientation.N -> Orientation.E
+            Orientation.E -> Orientation.S
+            Orientation.S -> Orientation.W
+            Orientation.W -> Orientation.N
+        }
+    }
+
+    fun turnLeft(robotActualOrientation: Orientation): Orientation{
+        return when(robotActualOrientation){
+            Orientation.N -> Orientation.W
+            Orientation.W -> Orientation.S
+            Orientation.S -> Orientation.E
+            Orientation.E -> Orientation.N
+        }
+    }
 
     /*
-    * TODO: logic of move
     * TODO: Rule that Robot position (X,Y) can never be higher than Workspace xMax, yMax or less than 0
     */
+
+    fun move(robotActualOrientation: Orientation, robotActualPosition: Position): Position {
+        return when(robotActualOrientation){
+            Orientation.N -> Position(robotActualPosition.x,robotActualPosition.y + 1)
+            Orientation.E -> Position(robotActualPosition.x + 1, robotActualPosition.y)
+            Orientation.S -> Position(robotActualPosition.x, robotActualPosition.y - 1)
+            Orientation.W -> Position(robotActualPosition.x - 1, robotActualPosition.y)
+        }
+    }
 
 }
 
