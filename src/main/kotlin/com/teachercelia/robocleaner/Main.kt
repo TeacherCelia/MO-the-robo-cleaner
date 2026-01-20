fbed5511d20e42fcb7349e3eae46d13d78c52020
devteachercelia <theteachercelia@gmail.com>
2026-01-20 23:11:36 +0100

    Oopsie woopsie... Junior error I guess... wrong file uploaded showing my PC route... Now yes, uploading Either library, and adding init rules to Workspace.

--- src/main/kotlin/com/teachercelia/robocleaner/Main.kt
+++ src/main/kotlin/com/teachercelia/robocleaner/Main.kt
@@ -3,6 +3,7 @@ package com.teachercelia.robocleaner
 import com.teachercelia.robocleaner.domain.MoTheRobot
 import com.teachercelia.robocleaner.domain.Orientation
 import com.teachercelia.robocleaner.domain.Position
+import com.teachercelia.robocleaner.domain.Workspace
 
 fun main(){
     // I will be using main for testing!
@@ -13,4 +14,8 @@ fun main(){
 
     println("The first robot is in position ${myFirstRobot.position.x},${myFirstRobot.position.y} and is looking at ${myFirstRobot.orientation}")
 
+    /* This throws an exception due to the Workspace 'init', I will decide if I keep this validation this way or not...
+    val myWorkspace = Workspace(-2, 4)
+    println(myWorkspace)
+     */
 }
--- src/main/kotlin/com/teachercelia/robocleaner/domain/Workspace.kt
+++ src/main/kotlin/com/teachercelia/robocleaner/domain/Workspace.kt
@@ -3,6 +3,9 @@ package com.teachercelia.robocleaner.domain
 data class Workspace (
     val xMax: Int,
     val yMax: Int
-)
-
-// TODO: Rule - these values can not be less than 0
+){
+    init {
+        require(xMax >= 0) {"X value is less than 0"}
+        require(yMax >= 0) {"Y value is less than 0"}
+    }
+}
