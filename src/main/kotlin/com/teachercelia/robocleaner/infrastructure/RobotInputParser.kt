b566f7caed2cfdec31aa4fd0e500c8a430bf7e4a
devteachercelia <theteachercelia@gmail.com>
2026-01-22 21:45:06 +0100

    Deleting my notes, and skipping refactor as... it works and is more important to start with validations!

--- src/main/kotlin/com/teachercelia/robocleaner/infrastructure/RobotInputParser.kt
+++ src/main/kotlin/com/teachercelia/robocleaner/infrastructure/RobotInputParser.kt
@@ -10,7 +10,6 @@ import com.teachercelia.robocleaner.domain.Workspace
 class RobotInputParser{
 
     fun parse(input: String): RobotInputCommand {
-        // 1 ----
         val lines = input
             .lines()
             .map { it.trim() }
@@ -18,7 +17,6 @@ class RobotInputParser{
         val workspaceLine = lines.getOrNull(0)
             ?: throw IllegalArgumentException("Empty input")
 
-        // 2 ----
         val workspaceParts = workspaceLine.split(Regex(" "))
         val xMax = workspaceParts[0].toInt()
         val yMax = workspaceParts[1].toInt()
@@ -32,22 +30,10 @@ class RobotInputParser{
             val positionLine = pair.getOrNull(0) ?: continue
             val instructionLine = pair.getOrNull(1) ?: ""
 
-            // -- parse robotLine: "x y O"
             val parts = positionLine.split(Regex(" "))
             val x = parts[0].toInt()
             val y = parts[1].toInt()
             val orientation = Orientation.valueOf(parts[2])
-
-            /*
-            for (c in instructionLine) {
-                when (c) {
-                    'L' -> robot.turnLeft()
-                    'R' -> robot.turnRight()
-                    'M' -> robot.move()
-                    else -> Unit
-                }
-            }
-             */
 
             val instructions = instructionLine.map { c ->
                 when (c) {
