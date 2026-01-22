3f4987f58932c3447f535ff06d0171f0364d38ad
devteachercelia <theteachercelia@gmail.com>
2026-01-22 20:26:14 +0100

    First input working! I used a "txt" file to read the input (easiest way to do it), and UseCase logic is done, pending refactor and/or split in other layers

--- src/main/kotlin/com/teachercelia/robocleaner/Main.kt
+++ src/main/kotlin/com/teachercelia/robocleaner/Main.kt
@@ -1,27 +1,19 @@
 package com.teachercelia.robocleaner
 
 import com.teachercelia.robocleaner.application.ExecuteRobotMovementUseCase
+import java.io.File
 
 fun main(){
     // I will be using main for testing!
 
-    // starting with the sample input without limits as a list of strings
-    val testOfInstructions = listOf(
-        "1 2 N",
-        "LMLMLMLMM",
-        "3 3 E",
-        "MMRMMRMRRM",
-        "2 3 S",
-        "MMRRMMLM"
-    )
+    // testing input from input.txt
+    val input = File("input.txt").readText()
 
-    // call the usecase
     val useCase = ExecuteRobotMovementUseCase()
-    val robots = useCase.executeRobotMovement(testOfInstructions)
+    val robots = useCase.executeRobotMovement(input)
 
-    // print each robot
-    robots.forEach {
-        println("${it.position.x} ${it.position.y} ${it.orientation}")
+    robots.forEach { robot ->
+        println("${robot.position.x} ${robot.position.y} ${robot.orientation}")
     }
 
 }
--- src/main/kotlin/com/teachercelia/robocleaner/application/ExecuteRobotMovementUseCase.kt
+++ src/main/kotlin/com/teachercelia/robocleaner/application/ExecuteRobotMovementUseCase.kt
@@ -3,42 +3,75 @@ package com.teachercelia.robocleaner.application
 import com.teachercelia.robocleaner.domain.MoTheRobot
 import com.teachercelia.robocleaner.domain.Orientation
 import com.teachercelia.robocleaner.domain.Position
+import com.teachercelia.robocleaner.domain.Workspace
 
 class ExecuteRobotMovementUseCase(
 ){
     fun executeRobotMovement(
-        //workspace: Workspace,
-        lines: List<String>
-    ): List<MoTheRobot> {
+        input: String
+    ): List<MoTheRobot>{
+        /*
+           1: divide input into lines
+           2: Save first line into workspace variable
+           3: Save as much robots as pairs of lines are in the lines input: robot + instructions
+           4: Execute movement of first robot, later others...
+           5: Give an output
+            - Limitations -
+            -- First line should be a workspace (2 items) --done!
+            -- Next lines should be pairs of robot + instructions --done!
+            ----> Robot limitations: it should be inside the workspace
+            ----> Instructions limitations: they should be L, R or M only
+            --> Extra limitation: each time robot moves, new position has to be inside workspace
+         */
+
+        // 1 -----
+        val lines = input
+            .lines() // divide text each time there is a line jump, in a list
+            .map { it.trim() } // delete spaces before and after text
+
+        // 2 -----
+        val workspaceLine = lines.getOrNull(0)
+            ?: throw IllegalArgumentException("Empty input") // null exception
+
+        val workspaceParts = workspaceLine.split(Regex(" "))
+        val xMax = workspaceParts[0].toInt()
+        val yMax = workspaceParts[1].toInt()
+        val workspace = Workspace(xMax, yMax)
+
+        // 3 -----
         val robots = mutableListOf<MoTheRobot>()
-        var i = 0
+        val robotLines = lines.drop(1) // drop() deletes first element of list
 
-        while (i < lines.size) {
-            val positionLine = lines[i]
-            val instructionLine = lines[i + 1]
+        val pairs = robotLines.chunked(2) // chunks() returns a pair list
 
-            // -- positionLine (robot) --
-            val parts = positionLine.split(" ")
+        for (pair in pairs) {
+            val positionLine = pair.getOrNull(0) ?: continue
+            val instructionLine = pair.getOrNull(1) ?: ""
+
+            // -- parse robotLine: "x y O"
+            val parts = positionLine.split(Regex(" "))
             val x = parts[0].toInt()
             val y = parts[1].toInt()
             val orientation = Orientation.valueOf(parts[2])
 
             val robot = MoTheRobot(Position(x, y), orientation)
 
-            // -- instructionsLine --
+            // 4 -----
             for (c in instructionLine) {
                 when (c) {
                     'L' -> robot.turnLeft()
                     'R' -> robot.turnRight()
                     'M' -> robot.move()
-                    else -> throw IllegalArgumentException("Invalid instruction, only L, R or M allowed.")
+                    else -> Unit
                 }
             }
 
             robots.add(robot)
-            i += 2
         }
+
+        // 5 -----
         return robots
+
     }
 
 }
